34548b8ea60c800168e3368efba62e8a0dbf819d
steve <stevens714@yahoo.com>
2020-10-05 11:01:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+#def oxford_comma(array)
+#array = "kiwi"
+#["kiwi"].join
+#end
 
-end
+
+#def oxford_comma_second
+#    array = "kiwi", "durian"
+#    ["kiwi, durian"].join("and")
+#end
+
+def oxford_comma(list)
+    return list.join(' and ') if list.size < 3
+    list[-1] = "and " + list[-1]
+    list.join(', ')
+  end 
+
+  #def oxford_comma(array)
+  #  if array.length == 2
+  #      array.join(" and ")
+  #  elsif array.length > 2
+  #      array.insert(-2, "and")
+  #      array[0..-1].join(", ")
+  #  else
+  #      array.join
+  #  end
+#end
